2d58907076f747e66e28a556bb22cbdc60db612a
JasonLantz <lantjason@yahoo.com>
2016-10-27 15:47:12 -0600

    chore: accomidate variation in main version

--- npm_scripts/release.js
+++ npm_scripts/release.js
@@ -26,7 +26,7 @@ const exitFailure = (message) => {
   process.exit(1);
 };
 
-if (branchName !== 'master' && branchName !== 'v0') {
+if (branchName !== 'master' && branchName.toLowerCase().charAt(0) !== 'v') {
     exitFailure('You must be on the master branch in order to execute a release.');
 }
 
