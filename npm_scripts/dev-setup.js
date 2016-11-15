397d3ac928ea6615e7e7f7a8f37c8bd3a0483b25
Jonathan Wyse <jonathan.wyse@pearson.com>
2016-11-15 09:46:21 -0700

    fix: update dev-setup to work cross platform

--- npm_scripts/dev-setup.js
+++ npm_scripts/dev-setup.js
@@ -1,4 +1,18 @@
+const path = require('path');
 const exec = require('./exec');
+const fs = require('fs-extra');
 
-exec(`mkdir -p fonts && cp ./node_modules/pearson-elements/dist/fonts/* ./fonts`);
-exec(`cp ./node_modules/pearson-elements/dist/css/elements.css ./demo`);
+exec(`mkdir -p fonts`);
+
+try {
+  fs.copySync(path.join(__dirname, '..', 'node_modules/pearson-elements/dist/fonts'), path.join(__dirname, '..', 'fonts'));
+} catch (err) {
+  console.error(err)
+}
+
+try {
+  fs.copy(path.join(__dirname, '..', 'node_modules/pearson-elements/dist/css/elements.css'), path.join(__dirname, '..', 'demo'));
+} catch (err) {
+  console.error(err)
+}
+
