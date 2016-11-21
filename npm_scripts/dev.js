f8c89904d5c8ec35c8daacba9716864b4c49fc11
Mike <Mhomolak@yahoo.com>
2016-11-21 15:12:16 -0700

    fix: Webpack fix to allow mobile access

--- npm_scripts/dev.js
+++ npm_scripts/dev.js
@@ -1,4 +1,4 @@
 const exec = require('./exec');
 
 exec(`npm run dev-setup`);
-exec(`webpack-dev-server --port 8081 --devtool source-map --hot --progress --colors`);
+exec(`webpack-dev-server --port 8081 --devtool source-map --hot --host 0.0.0.0 --progress --colors`);
